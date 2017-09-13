4756adfe6b731adda6b10842536266e23523f587
gnardini <gonzalo.nardini.dev@gmail.com>
2017-09-13 02:14:19 -0300

    Separate the stimulus mean calculation to its own funcion

--- Problem3.m
+++ Problem3.m
@@ -1,18 +1,8 @@
 load('c1p8.mat');
 result = [];
-% for tao = 1:150
 for tao = 1:150
-    current = zeros(tao,1);
-    totalShots = 0;
-    for i = (tao+1):size(rho)
-        if rho(i) == 1
-            totalShots = totalShots + 1;
-            startRange = i-tao;
-            endRange = i-1;
-            current = current + stim(startRange:endRange);
-        end
-    end
-    result = [result, mean(current)];
-%     result = current;
+    stimMean = StimulusMean(rho, stim, tao);
+    result = [result, mean(stimMean)];
+    result = stimMean;
 end
 plot(result)
---
+++ StimulusMean.m
@@ -0,0 +1,13 @@
+function stimMean = StimulusMean(rho, stim, tao)
+    stimMean = zeros(tao,1);
+    totalShots = 0;
+    for i = (tao+1):size(rho)
+        if rho(i) == 1
+            totalShots = totalShots + 1;
+            startRange = i-tao;
+            endRange = i-1;
+            stimMean = stimMean + stim(startRange:endRange);
+        end
+    end
+end
+
