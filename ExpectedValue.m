c14a520520b3149f8754d571df14cf7922ec435f
gnardini <gonzalo.nardini.dev@gmail.com>
2017-09-13 12:14:53 -0300

    Fixes expected value and variance calculations

--- ExpectedValue.m
+++ ExpectedValue.m
@@ -1,15 +1,10 @@
-function [expected, var, varCoef, fano] = ExpectedValue(shots)
+function [expected, variance, varCoef, fano] = ExpectedValue(shots)
     diffs = diff(shots);
-    total = sum(diffs);
-    length = size(shots);
-    length = length(2);
-    expected = total / length;
-    diffs = diffs.^2;
-    total = sum(diffs);
-    var = total / length - expected^2;
-    sigma = sqrt(var);
+    expected = mean(diffs);
+    variance = var(diffs);
+    sigma = sqrt(variance);
     % TODO: Hacerlo con los intervalos
     varCoef = sigma / expected;
-    fano = var / expected;
+    fano = variance / expected;
 end
 
