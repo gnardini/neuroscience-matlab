898e2648d9e91cd615272cbd7c70e238eea74f25
gnardini <gonzalo.nardini.dev@gmail.com>
2017-09-13 02:09:09 -0300

    Remove old problem 3 code

--- Problem3.m
+++ Problem3.m
@@ -1,23 +1,4 @@
 load('c1p8.mat');
-
-% Con Tao variable (vale el tiempo entre disparos):
-lastShot = 0;
-rangeStim = 0;
-averageStim = [];
-for i = 1:size(rho)
-    if rho(i) == 1
-        timeBetween = i - lastShot;
-        lastAverage = rangeStim / timeBetween;
-        averageStim = [averageStim, lastAverage];
-        rangeStim = 0;
-        lastShot = i;
-    else
-        rangeStim = rangeStim + stim(i);
-    end
-end
-mean(averageStim)
-
-% Con tao entre 0 y 300ms (150 saltos de tiempo):
 result = [];
 % for tao = 1:150
 for tao = 1:150
