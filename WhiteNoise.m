110f13f24a5e6f7ef6a1e54f279737d5d16f130d
gnardini <gonzalo.nardini.dev@gmail.com>
2017-09-13 12:15:56 -0300

    Fix white noise

--- WhiteNoise.m
+++ WhiteNoise.m
@@ -1,6 +1,19 @@
-function noise = WhiteNoise(time, dt)
-    noise = GenerateNoise(time, dt)
+function [noise, rks] = WhiteNoise(time, dt)
+    noise = GenerateNoise(time, dt) - .5;
     [exp, var, coef, fano] = ExpectedValue(noise);
+    rks = zeros(1,11);
+    for defasaje = 0:10
+        rks(defasaje+1) = Autocorrelation(noise, defasaje, exp);
+    end
+end
+
+function rk = Autocorrelation(noise, defasaje, exp)
+    steps = size(noise);
+    steps = steps(2);
+    rk = 0;
+    for i = (defasaje+1):steps
+        rk = rk + (noise(i-defasaje)-exp) * (noise(i)-exp);
+    end
 end
 
 function noise = GenerateNoise(time, dt)
