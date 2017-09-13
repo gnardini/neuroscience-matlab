1f84be3012ee2beeb3c86e18a3305c251a1134b2
gnardini <gonzalo.nardini.dev@gmail.com>
2017-09-13 12:15:40 -0300

    Fixes problem 3

--- Problem3.m
+++ Problem3.m
@@ -1,8 +1,2 @@
 load('c1p8.mat');
-result = [];
-for tao = 1:150
-    stimMean = StimulusMean(rho, stim, tao);
-    result = [result, mean(stimMean)];
-    result = stimMean;
-end
-plot(result)
+plot(StimulusMean(rho, stim, 150))
