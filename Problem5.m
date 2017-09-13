d6f7e8913d3049b61e2098562f880c5d023f9618
gnardini <gonzalo.nardini.dev@gmail.com>
2017-09-13 12:14:40 -0300

    Adds problem 5 solution

---
+++ Problem5.m
@@ -0,0 +1,26 @@
+correctness = zeros(11, 1);
+for d = 0:10
+    x = binornd(1, .5, 1000, 1);
+    stim = zeros(1000,1);
+    umbral = 20 + 5*d;
+    for i = 1:size(x)
+        e = normrnd(0, 10);
+        if x(i) == 0
+            stim(i) = 20 + e;
+        else
+            stim(i) = 20 + e + 10*d;
+        end
+        if stim(i) < 0
+            stim(i) = 0;
+        end
+        if stim(i) > umbral && x(i) == 1
+            stim(i) = 1;
+        elseif stim(i) < umbral && x(i) == 0
+            stim(i) = 1;
+        else
+            stim(i) = 0;
+        end
+    end
+    correctness(d+1) = mean(stim);
+end
+plot(correctness);
