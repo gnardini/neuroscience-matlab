707b5cee468c1e6b6e56f3967e7c89a0b7d79299
mariaeugenia <me.sakuda@hotmail.com>
2017-09-18 23:02:28 -0300

    Change poisson generator and spike generator

--- PoissonGenerator.m
+++ PoissonGenerator.m
@@ -1,14 +1,43 @@
-function shots = PoissonGenerator(time, rate)
+function [spikes, shots] = PoissonGenerator(time, rate, dt)
     shots = [];
     lastTime = 0;
-    while (lastTime < time)
-        lastTime = lastTime + NextTime(rate);
+    lambda = rate * dt;
+    while lastTime <= time
+        lastTime = lastTime + NextTime(lambda);
         if lastTime < time
             shots = [shots, lastTime];
         end
     end
+    spikes = GenerateSpikes(shots, time, dt);
 end
 
 function time = NextTime(rateParameter)
     time = -log(1 - rand) / rateParameter;
 end
+
+% function spikes = GenerateSpikes(shots, time, dt)
+%     spikes = [];
+%     shotPosition = 1;
+%     [row, shotSize] = size(shots);
+%     for i = 1:dt:time
+%         value = 0;
+%         if shotPosition <= shotSize
+%             if shots(shotPosition) < i
+%                 value = 1;
+%                 shotPosition = shotPosition + 1;
+%             end
+%         end
+%         spikes = [spikes, value];
+%     end
+% end
+
+
+function spikes = GenerateSpikes(shots, time, dt)
+    spikes = zeros(ceil(time / dt), 1);
+    for i = shots
+        j = ceil(i / dt);
+        aux = spikes(j);
+        spikes(j) = aux + 1;
+    end
+end
+
--- Problem1.m
+++ Problem1.m
@@ -1,3 +1,35 @@
-shots = PoissonGenerator(10, 100);
-[expected, var, varCoef, fano] = ExpectedValue(shots)
-histogram(diff(shots));
+expected = [];
+variance = [];
+sigma = [];
+varCoef = [];
+fano = [];
+for i = 1:1:100
+    deltaTime = i /1000.0; % pasar a segundos.
+    [spikes, times] = PoissonGenerator(10, 100, deltaTime);
+    diffs = diff(times);
+    mean1 = mean(spikes);
+    if ~isnan(mean1)
+        expected = [expected, mean1];
+        variance = [variance, var(spikes)];
+        sigma = [sigma, std(diffs)];
+        fanoo = var(spikes) / mean(spikes);
+%         Falta extrapolarlo a un gráfico. Arreglar un poco esto que es
+%         horrible
+        varCoef = [varCoef, std(diffs) / mean(diffs)]; 
+        fano = [fano, fanoo];
+    end
+end
+
+% histogram(acumulated, 100);
+
+% plot(varCoef, 'x');
+
+
+p = polyfit(expected, variance, 1)
+y1 = polyval(p,expected);
+  
+plot(expected, variance,'o');
+hold on 
+plot(expected,y1)
+hold off
+
