a1a30ec51afd1f3d11c2adb44fddae9b385d8801
mariaeugenia <sakuda.me@gmail.com>
2017-09-27 12:28:51 -0300

    Update error calculation problem 4

--- Problem4.m
+++ Problem4.m
@@ -1,7 +1,7 @@
 load('c1p8.mat');
 tao = 150;
 C_t = StimulusMean(rho, stim, tao);
-r_avg = sum(rho) / (20*60*1000);
+r_avg = sum(rho) * 2 / (20*60*1000); 
 sigma_sq = var(stim);
 D_t = C_t * r_avg / sigma_sq;
 
@@ -14,9 +14,9 @@ end
 r0 = r_avg - mean(L_t);
 r_est = r0 + L_t;
 
-pred_spikes = GenerateSpikes(r_est);
+pred_spikes = GenerateSpikes(r_est); % Por qué funciona con un 3 ahí?!
 real_spikes = rho(tao+1:600000);
-interval = 50;
+interval = 300;
 total = floor(length(r_est)/interval)-1;
 err = zeros(total, 1);
 for i = 1:total
@@ -24,6 +24,14 @@ for i = 1:total
     end_ = start + interval;
     pred_sum = sum(pred_spikes(start:end_));
     real_sum = sum(real_spikes(start:end_));
-    err(i) = abs(pred_sum-real_sum);
+    err(i) = (pred_sum-real_sum) / interval;
 end
-mean(err) / interval
+plot(err);
+
+% Plot for compare spikes in np samples.
+figure();
+np=300;
+subplot(2,1,1)
+stem(stim(1:np),real_spikes(1:np),'r');  % verdaderos
+hold on
+stem(stim(1:np),pred_spikes(1:np),'b'); % generados
