2e72fcfb446412fae55f084329f2f7653d62ec74
eugis <sakuda.me@gmail.com>
2017-10-09 21:40:45 -0300

    Fix problem 2a and make 2b

--- TP2/Problem2.m
+++ TP2/Problem2.m
@@ -1,23 +1,42 @@
 % Problem 2 code for part a
 % I should be in nA
 Iapp = [0.01:0.001:3].* 10e-9;
-rates = zeros(size(Iapp));
-for i = 1:length(rates)
-    rates(i) = Rate(Iapp(i));
+rates_i = zeros(size(Iapp));
+
+Gl = [0.01:0.001:3].*10e9;
+rates_g = zeros(size(Gl));
+
+for i = 1:length(rates_i)
+    rates_i(i) = Rate_i(Iapp(i));
+    rates_g(i) = Rate_g(Gl(i));
 end
-plot(Iapp, rates);
+
+figure();
+plot(Iapp, rates_i);
+
+figure();
+plot(Gl, rates_g);
 
 % TODO: Revisar
-function r = Rate(i)
+function r = Rate_i(i)
     Rm = 10e6;
     Vreset = -80e-3;
     Vth = -40e-3;
     Ve = -70e-3;
     Taum = 10e-3;
     r = 0;
-    if (Vreset < Ve + i * Rm)
+    if (Vth < Ve + i * Rm)
         r = (-Taum * log((Vth - Ve - i * Rm) / ((Vreset - Ve - i * Rm))))^-1;
     end
 end
 
 % Problem 2 code for part b
+function r = Rate_g(g)
+%     Vreset = -80e-3;
+    Vreset = -60e-3;
+    Vth = -40e-3;
+    Ve = -70e-3;
+    Cm = 10e-3 / 10e6;
+    l = log((Vth - Ve) / (Vreset - Ve));
+    r = (-Cm / g * l)^-1;
+end
